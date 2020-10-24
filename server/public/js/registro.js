fda2bb2445a3663d2fa1e6208c2b1f8bdc43534d
Jesus Mejia <jesmga24@gmail.com>
2020-10-23 20:12:00 -0500

    Added same password confirmation and modified SAT logo image

---
+++ server/public/js/registro.js
@@ -0,0 +1,31 @@
+function init(){
+    let password = document.querySelector("#password");
+    let confPassword = document.querySelector("#confPassword");
+
+    password.addEventListener('focusout', (event) => {
+        event.preventDefault();
+        if (password.value != confPassword.value) {
+            confPassword.classList.remove("valid");
+            confPassword.classList.add("invalid");
+        } else {
+            confPassword.classList.remove("invalid")
+            confPassword.classList.add("valid");
+        }
+      });
+
+    confPassword.addEventListener("keyup", (event) => {
+        event.preventDefault();
+        if (password.value != confPassword.value) {
+            confPassword.classList.remove("valid");
+            confPassword.classList.add("invalid");
+        } else if (password.value != ""){
+            confPassword.classList.remove("invalid");
+            confPassword.classList.add("valid");
+        } else{
+            confPassword.classList.remove("invalid");
+            confPassword.classList.remove("valid");
+        }
+    })
+}
+
+init();
