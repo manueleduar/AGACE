093e5abd571c58c532ab9a2f6be8305894ba7ebd
manueleduar <manuel_e123@hotmail.com>
2020-11-08 15:05:30 -0600

    Detalles-

--- public/js/seguimiento.js
+++ public/js/seguimiento.js
@@ -7,7 +7,7 @@ function load(){
         data.forEach(element => {
             $("#denuncias").append(
                 '<tr>'+
-                    '<td>'+element.tema.nombre+'</td>'+
+                    '<td>'+element.tema.nombre+'</td>'+ 
                     '<td>'+element.descripcion+'</td>'+
                     '<td>'+element.admasignada+'</td>'+
                     '<td>'+element.rfcs.length+'</td>'+
@@ -15,7 +15,7 @@ function load(){
                     '<td>'+element.admlider+'</td>'+
                     '<td>'+element.rfcs.length+'</td>'+
                     '<td>'+element.avanceaccpce+'%</td>'+
-                    '<td> <a class="waves-effect waves-light btn">Editar</a></td>'+
+                    '<td> <a class="waves-effect waves-light btn">Detalles</a></td>'+
                 '</tr>'
             )
 
@@ -25,33 +25,3 @@ function load(){
 }
 
 load();
-
-let generarReportesBtn = document.getElementById("generar-reportes");
-
-if (generarReportesBtn){
-    generarReportesBtn.addEventListener('click',(event) => {
-        var request = new XMLHttpRequest();
-        request.open('GET', '/api/reportes', true);
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        request.responseType = 'blob';
-        
-        request.onload = function(e) {
-            if (this.status === 200) {
-                var blob = this.response;
-                if(window.navigator.msSaveOrOpenBlob) {
-                    window.navigator.msSaveBlob(blob, 'Reporte.xlsx');
-                }
-                else{
-                    var downloadLink = window.document.createElement('a');
-                    var contentTypeHeader = request.getResponseHeader("Content-Type");
-                    downloadLink.href = window.URL.createObjectURL(new Blob([blob], { type: contentTypeHeader }));
-                    downloadLink.download = 'Reporte.xlsx';
-                    document.body.appendChild(downloadLink);
-                    downloadLink.click();
-                    document.body.removeChild(downloadLink);
-                   }
-               }
-           };
-           request.send();
-    }) 
-}
